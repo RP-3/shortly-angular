45f2f4d77d4cfd2d110986b3f576879fa10f1056
RP-3 <sarith21@gmail.com>
2014-05-16 23:21:39 -0700

    Implements equivalent basic functionality to Backbone version

--- public/client/js/app.js
+++ public/client/js/app.js
@@ -1,1 +1,50 @@
+angular.module('shortly', ['ngRoute'])
+.config(
+  function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'client/templates/home.html',
+        controller: 'linkController',
+        controllerAs: 'link'
+      })
+      .when('/create', {
+        templateUrl: 'client/templates/shorten.html',
+        controller: 'createController',
+        controllerAs: 'creator'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
 
+  }
+)
+
+.controller('mainController', function($route, $routeParams, $location) {
+  this.$route = $route;
+  this.$location = $location;
+  this.$routeParams = $routeParams;
+})
+
+.controller('linkController', function($http) {
+  var self = this;
+  $http({
+    method: 'GET',
+    url: '/links'
+  }).success(function(data) {
+    self.links = data;
+  });
+})
+
+.controller('createController', function($scope, $http) {
+  $scope.submit = function() {
+    if ($scope.text) {
+      $http({
+        method: 'POST',
+        url: '/links',
+        data: {url: $scope.text}
+      }).success(function(data) {
+        console.log("posted! ", data)
+      });
+    }
+  };
+});
